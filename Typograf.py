ba1b5887ecd4138195772791de6c071908965539
Shilov Vlad <omgovich@ya.ru>
2013-10-17 00:11:53 +0400

    Text replacing

--- Typograf.py
+++ Typograf.py
@@ -2,4 +2,11 @@ import sublime, sublime_plugin
 
 class TypografCommand(sublime_plugin.TextCommand):  
 	def run(self, edit):
-		print('Sceleton')
+		for region in self.view.sel():
+			if not region.empty():
+				# Get the selected text
+				s = self.view.substr(region)
+				# Replace text
+				s = s.replace(' ', '')
+				# Replace the selection with transformed text
+				self.view.replace(edit, region, s)
