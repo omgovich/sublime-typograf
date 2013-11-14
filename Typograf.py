601a9c82385b193337e527b06a5970589f0c4d10
Shilov Vlad <omgovich@ya.ru>
2013-11-14 23:06:24 +0400

    Valid response and replace selection

--- Typograf.py
+++ Typograf.py
@@ -6,7 +6,8 @@ class TypografCommand(sublime_plugin.TextCommand):
 		for region in self.view.sel():
 			if not region.empty():
 				selection = self.view.substr(region)
-				params = urllib.parse.urlencode({ 'text':selection })
+				params = urllib.parse.urlencode({ 'text': selection, 'chr': 'UTF-8' })
 				params = params.encode('windows-1251')
 				response = urllib.request.urlopen(url, params)
-				self.view.replace(edit, region, str(response.read()))
+				lines = response.read().decode("utf-8");
+				self.view.replace(edit, region, lines)
