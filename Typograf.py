87469bce374a34ae5ba03647507ee54ba723e2e5
Shilov Vlad <omgovich@ya.ru>
2013-11-14 23:39:59 +0400

    Disabling tags in text

--- Typograf.py
+++ Typograf.py
@@ -1,12 +1,15 @@
 import sublime, sublime_plugin, urllib
 
-class TypografCommand(sublime_plugin.TextCommand):  
-	def run(self, edit):
+class TypografCommand(sublime_plugin.TextCommand):
+
+	def run(self, edit, **args):
 		url = 'http://www.typograf.ru/webservice/'
+		xml = '<?xml version="1.0" encoding="windows-1251" ?><preferences><tags delete="0">' + args['tags'] + '</tags></preferences>'
+		print(xml)
 		for region in self.view.sel():
 			if not region.empty():
 				selection = self.view.substr(region)
-				params = urllib.parse.urlencode({ 'text': selection, 'chr': 'UTF-8' })
+				params = urllib.parse.urlencode({ 'text': selection, 'chr': 'UTF-8', 'xml': xml })
 				params = params.encode('windows-1251')
 				response = urllib.request.urlopen(url, params)
 				lines = response.read().decode("utf-8");
