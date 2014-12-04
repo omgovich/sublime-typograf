00ed82c0e27c651a8671ceb9721d6f27daa7d5c1
Shilov Vlad <omgovich@ya.ru>
2014-12-05 00:37:34 +0400

    Paste from clipboard

--- Typograf.py
+++ Typograf.py
@@ -6,10 +6,10 @@ class TypografCommand(sublime_plugin.TextCommand):
 		url = 'http://www.typograf.ru/webservice/'
 		xml = '<?xml version="1.0" encoding="windows-1251" ?><preferences><tags delete="0">' + args['tags'] + '</tags></preferences>'
 		for region in self.view.sel():
-			if not region.empty():
-				selection = self.view.substr(region)
-				params = urllib.parse.urlencode({ 'text': selection, 'chr': 'UTF-8', 'xml': xml })
+			source =  sublime.get_clipboard() if args['paste'] else self.view.substr(region)
+			if source:
+				params = urllib.parse.urlencode({ 'text': source, 'chr': 'UTF-8', 'xml': xml })
 				params = params.encode('windows-1251')
 				response = urllib.request.urlopen(url, params)
-				lines = response.read().decode("utf-8");
+				lines = response.read().decode("utf-8")
 				self.view.replace(edit, region, lines)
